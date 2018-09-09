409a1ac22fed8b59c8d029e1938ed6ae4c7b122a
David Estes McKnight <belmont95@live.com>
2018-09-09 17:41:21 -0500

    Add project files.

---
+++ PokemonSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokemonSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
